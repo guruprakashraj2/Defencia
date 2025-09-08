6b763942ffa29f85b2a97baf505f42690d071e8c
Guruprakashraj <essakiprakash26@gmail.com>
2025-09-08 15:27:20 +0530

    Show message field in Contact admin

--- App/admin.py
+++ App/admin.py
@@ -5,6 +5,6 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ("name", "email", "created_at")
+    list_display = ("name", "email", "created_at","message")
     search_fields = ("name", "email", "message")
     list_filter = ("created_at",)
